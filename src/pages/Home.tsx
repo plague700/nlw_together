919c34aabcc34ae8c5b516bdea9b6de3a075c154
plaque700 <marcus-braga@hotmail.com>
2021-06-27 10:41:27 -0300

    Processo para entrar em uma sala existente com trativa para validar se a mesma existe caso sim realizar um redirect

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg';
@@ -6,6 +7,8 @@ import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
 
+import { database } from '../services/firebase';
+
 import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
@@ -13,6 +16,7 @@ import '../styles/auth.scss';
 export function Home() {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState('');
 
   async function handleCreateRoom(){
     if(!user){
@@ -20,6 +24,24 @@ export function Home() {
     }
 
     history.push('/rooms/new');
+  }
+
+  async function handleJoinRoom(event: FormEvent){
+    event.preventDefault();
+
+    if(roomCode.trim() === ''){
+      return;
+    }
+
+    const roomRef = await database.ref('rooms/'+roomCode).get();
+
+    if(!roomRef.exists()){
+      alert('A sala não existe!');
+      return;
+    }
+
+    history.push('rooms/'+roomCode);
+    
   }
 
   return (
@@ -39,10 +61,12 @@ export function Home() {
             Crie sua sala com o Google
           </button>
           <div className="separator">ou entre em uma sala</div>
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input 
               type="text"
               placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button type="submit">
               Entrar na sala
