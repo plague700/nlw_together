80651d30c00d96f4a18bc9a0982d0c84f7f0f6f7
plaque700 <marcus-braga@hotmail.com>
2021-07-04 15:16:25 -0300

    Encerramento de sala via admin e retorno de alert para informar que sala esta encerrada

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 import logoImg from '../assets/images/logo.svg';
 import deleteImg from '../assets/images/delete.svg';
@@ -19,10 +19,20 @@ type RoomParams = {
 
 export function AdminRoom(){
   // const {user} = useAuth();
+  const history = useHistory();
   const params = useParams<RoomParams>();
   const roomId = params.id;
 
   const {title, questions} = useRoom(roomId);
+
+  async function handleEndRoom() {
+    await database.ref(`rooms/${roomId}`).update({
+      endedAt: new Date(),
+    });
+
+    //Redirecionamento
+    history.push('/');
+  }
 
   async function handleDeleteQuestion(questionId: string){
     if(window.confirm('Tem certeza que você deseja excluir esta pergunta?')){
@@ -37,7 +47,12 @@ export function AdminRoom(){
           <img src={logoImg} alt="Letmeask"/>
           <div>
             <RoomCode code={roomId}/>
-            <Button isOutlined>Encerrar Sala</Button>
+            <Button 
+              isOutlined
+              onClick={handleEndRoom}
+            >
+              Encerrar Sala
+            </Button>
           </div>
         </div>
       </header>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -40,6 +40,11 @@ export function Home() {
       return;
     }
 
+    if(roomRef.val().endedAt) {
+      alert('A sala foi encerrada!');
+      return;
+    }
+
     history.push('rooms/'+roomCode);
     
   }
